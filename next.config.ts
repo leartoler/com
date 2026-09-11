68f29c3ad9c88a3c8b7afd5f833010b22d679210
Gosha_sans <fabiangg@protonmail.ch>
2026-09-11 09:26:59 -0600

    cambios en el direccionamiento

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,21 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return {
       beforeFiles: [
-        
+        {
+          source: '/',
+          destination: '/cultura.html',
+          has: [{ type: 'host', value: 'cultura.0x000042.com' }],
+        },
+        {
+          source: '/',
+          destination: '/desaparecidos.html',
+          has: [{ type: 'host', value: 'desaparecidos.0x000042.com' }],
+        },
+        {
+          source: '/',
+          destination: '/clima.html',
+          has: [{ type: 'host', value: 'clima.0x000042.com' }],
+        },
         {
           source: '/',
           destination: '/ausencias.html',
